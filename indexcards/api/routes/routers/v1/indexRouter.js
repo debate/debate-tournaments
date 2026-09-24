a1eb9419a00387813a485bd4708811df7de20daf
Ryan Tullis <tulliscryan@gmail.com>
2026-09-24 09:45:49 -0500

    remove ext routes

--- indexcards/api/routes/routers/v1/indexRouter.js
+++ indexcards/api/routes/routers/v1/indexRouter.js
@@ -2,10 +2,7 @@ import { Router } from 'express';
 import { fileURLToPath } from 'node:url';
 
 import authRouter from './authRouter.js';
-import adminRouter from './admin/adminRouter.js';
-import extRouter from './ext/extRouter.js';
 import pagesRouter from './pages/pagesRouter.js';
-import tabRouter from './tab/indexRouter.js';
 import restRouter from './rest/restRouter.js';
 import { apiReference } from '@scalar/express-api-reference';
 
@@ -13,20 +10,10 @@ import { apiReference } from '@scalar/express-api-reference';
 import statusRouter from './admin/statusRouter.js';
 
 import legacyUserRouter from './legacy/userRouter.js';
-import legacyCoachRouter from './legacy/coachRouter.js';
 import legacyPublicRouter from './legacy/public/indexRouter.js';
-import { requireLogin, requireSiteAdmin } from '../../../middleware/authorization/authorization.js';
-import config from '../../../config.js';
+import { requireLogin } from '../../../middleware/authorization/authorization.js';
 
 const router = Router({ mergeParams: true });
-
-// hide in progress endpoints behind a flag, so that we don't have to worry about them
-if (!config.features.HIDE_DEV_ENDPOINTS) {
-	router.use('/coach' , legacyCoachRouter);
-	router.use('/tab'   , tabRouter);
-	router.use('/admin' , requireSiteAdmin, adminRouter);
-	router.use('/ext'   , extRouter);
-}
 
 router.use('/pages'  , pagesRouter);
 router.use('/rest'   , restRouter);
